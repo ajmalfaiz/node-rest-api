3686ee18ff1545b28d968015b45a74a73f2aa2b5
Ajmalfaiz <ajmalfaiz5@gmail.com>
2020-05-29 13:17:40 +0530

    first

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/product");
+const orderRoutes = require("./api/routes/order");
+
+//mongoose connect
+mongoose.connect(
+  "mongodb+srv://ajmalfaiz:" +
+    process.env.MONGO_ATLAS_PWD +
+    "@node-rest-shop-vydm3.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, , Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+//Handling Routes
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+//Error Handling
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
